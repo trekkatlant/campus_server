876ef7b2390913513103bc1576b8a1f592d5880f
Bobur <bobur.hu0991@gmail.com>
2020-01-10 15:27:23 -0500

    models and schemas setup

--- database/models/campus.js
+++ database/models/campus.js
@@ -2,8 +2,25 @@ const Sequelize = require("sequelize");
 const db = require("../db");
 
 const Campus = db.define('campus', {
-
-
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    imageUrl: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: "https://www.uri.edu/wp-content/uploads/home/2018/05/kingston-campus-500x327.jpg"   
+    },
+    address: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+},{
+    timestamps: false
 });
 
 module.exports = Campus;
--- database/models/student.js
+++ database/models/student.js
@@ -2,8 +2,28 @@ const Sequelize = require("sequelize");
 const db = require("../db");
 
 const Student = db.define('student', {
-
-
+    firstName : {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    lastName: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    imageUrl : {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: "https://www.trustedclothes.com/blog/wp-content/uploads/2019/02/anonymous-person-221117.jpg"
+    },
+    gpa: {
+        type: Sequelize.DECIMAL(10,1)
+    }
+},{
+    timestamps: false
 });
 
 module.exports = Student;
