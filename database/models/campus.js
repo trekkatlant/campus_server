7d13df556b21bd2820db53fd3715ea6d409db7c9
Bobur <bobur.hu0991@gmail.com>
2020-01-10 15:06:07 -0500

    db schemes init

---
+++ database/models/campus.js
@@ -0,0 +1,9 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const Campus = db.define('campus', {
+
+
+});
+
+module.exports = Campus;
---
+++ database/models/student.js
@@ -0,0 +1,9 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const Student = db.define('student', {
+
+
+});
+
+module.exports = Student;
