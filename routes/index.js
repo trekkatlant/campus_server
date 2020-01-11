477a62572af6c60696cddb44cd45fa9fa2a9e1b8
Bobur <bobur.hu0991@gmail.com>
2020-01-11 11:29:51 -0500

    routes index setup

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const studentRoute = require("./student")
+const campusRoute = require("./campus");
+
+router.use("/students", studentRoute);
+router.use("/campuses", campusRoute);
+
+router.use((req, res, next) => {
+    res.status(200).send("Default API, nothing to see here");
+})
+
+module.exports = router;
