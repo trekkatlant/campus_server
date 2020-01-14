19a25e0631e5de4a3be3c5dc356762773207b32d
Bobur <bobur.hu0991@gmail.com>
2020-01-14 14:47:03 -0500

    dummy data written

---
+++ seed/index.js
@@ -0,0 +1,5 @@
+const Student = require("../database/models/student");
+const Campus = require("../database/models/campus");
+const students = require("./student");
+const campuses = require("./campus");
+
