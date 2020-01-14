50e957faa7367ef5bda4f0f7afa26daee7e30f26
Bobur <bobur.hu0991@gmail.com>
2020-01-14 14:41:57 -0500

    dummy data written

--- seed/dummyCampuses.js
+++ seed/dummyCampuses.js
@@ -1,26 +1,21 @@
-            // name: req.body.name,
-            // imageUrl: req.body.imageUrl,
-            // address: req.body.address,
-            // description: req.body.description
-
 let campuses = [
     {
-        name : "",
-        imageUrl : "",
-        address : "",
-        description : ""
+        name : "Hunter College",
+        imageUrl : "https://www.gonzaga.edu/-/media/Website/Images/Full-Width-Image-and-Video/About/Other/fw-flyover-video-screenshot.ashx?h=600&w=1440&la=en&hash=2BB5F333CB2B74543077126EAB3907907D5F290F",
+        address : "695 Park Ave, New York, NY 10065",
+        description : "College in New York City, New York"
     },
     {
-        name : "",
-        imageUrl : "",
-        address : "",
-        description : ""
+        name : "Harvard University",
+        imageUrl : "https://www.gonzaga.edu/-/media/Website/Images/Full-Width-Image-and-Video/About/Other/fw-flyover-video-screenshot.ashx?h=600&w=1440&la=en&hash=2BB5F333CB2B74543077126EAB3907907D5F290F",
+        address : "Cambridge, MA",
+        description : "Private university in Cambridge, Massachusetts"
     },
     {
-        name : "",
-        imageUrl : "",
-        address : "",
-        description : ""
+        name : "DeVry University",
+        imageUrl : "https://www.gonzaga.edu/-/media/Website/Images/Full-Width-Image-and-Video/About/Other/fw-flyover-video-screenshot.ashx?h=600&w=1440&la=en&hash=2BB5F333CB2B74543077126EAB3907907D5F290F",
+        address : "3300 N Campbell Ave, Chicago, IL 60618",
+        description : "University in Chicago, Illinois"
     }
 ]
 
--- seed/dummyStudents.js
+++ seed/dummyStudents.js
@@ -1,9 +1,3 @@
-            // firstName: req.body.firstName,
-            // lastName: req.body.lastName,
-            // email : req.body.email,
-            // imageUrl : req.body.imageUrl,
-            // gpa: req.body.gpa
-
 let students = [
     {
         firstName: "Bobby",
@@ -15,37 +9,37 @@ let students = [
     {
         firstName: "Nariman",
         lastName : "Grigorean",
-        email: "designer@mail.ru",
+        email: "designer@umail.com",
         imageUrl: "https://nisd.net/sites/default/files/graphics/students/kinderB.jpg",
         gpa: 3.5
     },
     {
-        firstName: "",
-        lastName : "",
-        email: "",
+        firstName: "Shinya",
+        lastName : "Kogami",
+        email: "dtec@dept.gov",
         imageUrl: "https://nisd.net/sites/default/files/graphics/students/kinderB.jpg",
-        gpa:
+        gpa: 3.7
     },
     {
-        firstName: "",
-        lastName : "",
-        email:  "",
+        firstName: "Tony",
+        lastName : "Fung",
+        email:  "tfung@gmail.com",
         imageUrl: "https://nisd.net/sites/default/files/graphics/students/kinderB.jpg",
-        gpa:
+        gpa: 3.99
     },
     {
-        firstName: "",
-        lastName : "",
-        email: "",
+        firstName: "Habib",
+        lastName : "Nurmagamedov",
+        email: "habibu@mail.ru",
         imageUrl: "https://nisd.net/sites/default/files/graphics/students/kinderB.jpg",
-        gpa:
+        gpa: 3.0
     },
     {
-        firstName: "",
-        lastName : "",
-        email: "",
+        firstName: "Student",
+        lastName : "McStudentface",
+        email: "irishman@gmail.com",
         imageUrl: "https://nisd.net/sites/default/files/graphics/students/kinderB.jpg",
-        gpa:
+        gpa: 1.5
     }
 ]
 
