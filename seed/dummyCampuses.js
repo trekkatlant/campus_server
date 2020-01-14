c48e673608a22480d1bfe8f95f3b796a52949672
Bobur <bobur.hu0991@gmail.com>
2020-01-14 14:27:25 -0500

    seeding

--- seed/dummyCampuses.js
+++ seed/dummyCampuses.js
@@ -1,19 +1,27 @@
+            // name: req.body.name,
+            // imageUrl: req.body.imageUrl,
+            // address: req.body.address,
+            // description: req.body.description
+
 let campuses = [
     {
-
+        name : "",
+        imageUrl : "",
+        address : "",
+        description : ""
     },
     {
-
+        name : "",
+        imageUrl : "",
+        address : "",
+        description : ""
     },
     {
-
-    },
-    {
-
-    },
-    {
-
-    },
+        name : "",
+        imageUrl : "",
+        address : "",
+        description : ""
+    }
 ]
 
 module.exports = campuses;
--- seed/dummyStudents.js
+++ seed/dummyStudents.js
@@ -1,19 +1,52 @@
+            // firstName: req.body.firstName,
+            // lastName: req.body.lastName,
+            // email : req.body.email,
+            // imageUrl : req.body.imageUrl,
+            // gpa: req.body.gpa
+
 let students = [
     {
-
+        firstName: "Bobby",
+        lastName : "Hall",
+        email: "logical@aol.com",
+        imageUrl: "https://nisd.net/sites/default/files/graphics/students/kinderB.jpg",
+        gpa: 2.34
     },
     {
-
+        firstName: "Nariman",
+        lastName : "Grigorean",
+        email: "designer@mail.ru",
+        imageUrl: "https://nisd.net/sites/default/files/graphics/students/kinderB.jpg",
+        gpa: 3.5
     },
     {
-
+        firstName: "",
+        lastName : "",
+        email: "",
+        imageUrl: "https://nisd.net/sites/default/files/graphics/students/kinderB.jpg",
+        gpa:
     },
     {
-
+        firstName: "",
+        lastName : "",
+        email:  "",
+        imageUrl: "https://nisd.net/sites/default/files/graphics/students/kinderB.jpg",
+        gpa:
     },
     {
-
+        firstName: "",
+        lastName : "",
+        email: "",
+        imageUrl: "https://nisd.net/sites/default/files/graphics/students/kinderB.jpg",
+        gpa:
     },
+    {
+        firstName: "",
+        lastName : "",
+        email: "",
+        imageUrl: "https://nisd.net/sites/default/files/graphics/students/kinderB.jpg",
+        gpa:
+    }
 ]
 
 module.exports = students;
