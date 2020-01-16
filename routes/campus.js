7292dafb193b0994d75d9c6a4ada9d67fed63098
Bobur <bobur.hu0991@gmail.com>
2020-01-16 10:59:38 -0500

    Revert "Revert "heroku""
    This reverts commit 0171b1c21232832f2ecd931a2bf0a32e1b2e50ec.

--- routes/campus.js
+++ routes/campus.js
@@ -2,6 +2,7 @@ const bodyParser = require("body-parser");
 const campus = require("express").Router();
 const db = require("../database/db");
 const { Campus, Student } = require("../database/models");
+const db = require("../database/db");
 campus.use(bodyParser.json());
 
 //gets all campuses
@@ -25,7 +26,7 @@ campus.get("/:id", async(req, res) => {
         if(data) {
             res.status(200).json(data);
         } else {
-            res.status(400).json("Campus not found");
+            res.status(400).send("Campus not found");
         }
     } catch(err) {
         res.status(400).send(err);
@@ -57,13 +58,13 @@ campus.post('/', async(req, res, next) => {
             description : req.body.description
         })
         if(data) {
-            res.status(201).json("New campus added successfully");
+            res.status(201).send("New campus added successfully");
         } else {
-            res.status(400).json("not added");
+            res.status(400).send("not added");
         }
 
     } catch(err) {
-        res.status(400).json(err);
+        res.status(400).send(err);
     }
 });
 //update campus with id
@@ -77,9 +78,9 @@ campus.put("/:id", async(req, res) => {
         },
         { where: {id: req.params.id}})
         if(data) {
-            res.status(200).json("Update successful");
+            res.status(200).send(data[1]);
         } else {
-            res.status(400).json("Updata unsuccessful");
+            res.status(400).send("Updata unsuccessful");
         }
     } catch(err) {
         res.status(400).send(err);
@@ -90,9 +91,9 @@ campus.delete("/:id", async(req, res) => {
     try {
         let data = await Campus.destroy({ where: {id: req.params.id }});
         if(data) {
-            res.status(200).json("Delete successful");
+            res.status(200).send("Delete successful");
         } else {
-            res.status(400).json("Delete unsuccessful");
+            res.status(400).send("Delete unsuccessful");
         }
     } catch(err) {
         res.status(400).send(err);
--- routes/student.js
+++ routes/student.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const student = express.Router();
 const { Campus, Student } = require("../database/models");
 student.use(bodyParser.json());
+const db = require("../database/db");
 
 //get all students
 student.get("/", async(req, res) => {
@@ -15,7 +16,7 @@ student.get("/", async(req, res) => {
             res.status(400).send("No students");
         }
     } catch(err) {
-        res.status(400).json(err);
+        res.status(400).send(err);
     }
 });
 //get student with id
@@ -54,7 +55,7 @@ student.post("/", async(req, res) => {
             imageUrl : req.body.imageUrl,
             gpa: req.body.gpa
         })
-        await data.setCampus(req.body.campus);
+        // await data.setCampus(req.body.campus);
         if(data) {
             res.status(201).send("New student added successfully");
         } else {
@@ -77,9 +78,9 @@ student.put("/:id", async(req, res) => {
         },
         { where: {id: req.params.id}})
         if(data) {
-            res.status(200).json("Update successful");
+            res.status(200).send(data[1]);
         } else {
-            res.status(400).json("Update unsuccessful");
+            res.status(400).send("Update unsuccessful");
         }
     } catch(err) {
         res.status(400).send(err);
